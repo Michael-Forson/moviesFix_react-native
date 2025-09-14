6bb5aada5abb545ceaa2b2852190a851daa62f59
Mike <micfor722@gmail.com>
2025-09-14 15:37:05 +0000

    searchingData

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -46,11 +46,12 @@ export default function Index() {
         ) : (
           <View className="flex-1 mt-5 ">
             <SearchBar
-              onPress={() => {
-                router.push("/search");
-              }}
-              placeholder="Search for a movie"
-            />
+                  onPress={() => {
+                    router.push("/search");
+                  } }
+                  placeholder="Search for a movie" value={""} onChangeText={function (text: string): void {
+                    throw new Error("Function not implemented.");
+                  } }            />
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3 ">
                 {" "}
